0b7cd402be688316b7bfbbcf61c40c183a9ac5a4
Stoyan Rachev <stoyanr@gmail.com>
2012-12-15 16:14:30 +0200

    Updated javadoc and binaries

--- publish_javadoc.bat
+++ publish_javadoc.bat
@@ -2,7 +2,7 @@ rem @echo off
 
 call git checkout master
 
-call mvn javadoc:javadoc
+call mvn clean install -DskipTests
 
 call git checkout gh-pages
 
@@ -10,10 +10,11 @@ cd evictor
 rd /s /q javadoc
 md javadoc
 xcopy /s target\site\apidocs\* javadoc
+xcopy /s target\*.jar lib
 cd ..
 
 call git add .
-call git commit -m "Updated javadoc"
+call git commit -m "Updated javadoc and binaries"
 call git push origin gh-pages
 
 call git checkout master
